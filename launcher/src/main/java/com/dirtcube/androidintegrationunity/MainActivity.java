b67d678c86bb7e6f05cb4c29728abaa3924901ad
DeeshaRv <deesha@creativegalileo.com>
2021-03-31 14:52:26 +0530

    Added the java functions to be called at various game event. Read the function description for the details.

--- launcher/src/main/java/com/dirtcube/androidintegrationunity/MainActivity.java
+++ launcher/src/main/java/com/dirtcube/androidintegrationunity/MainActivity.java
@@ -50,8 +50,60 @@ public class MainActivity extends UnityPlayerActivity {
         return null;
     }
 
-    /*Call this event */
-    public void gameStarted(String levelNumber){
+    /*Call this event when game play of a new level starts
+    * VAR: levelNumber is the numeric value of the level started
+    * CALL THIS FUNCTION AGAIN IF THE LEVEL HAS RE-STARTED */
+    public void gameStarted(int levelNumber){
+
+    }
+
+    /*Call this event when game play of a level is completed successfully
+     * VAR: levelNumber is the numeric value of the level completed*/
+    public void gameCompleted(int levelNumber){
+
+    }
+
+    /*Call this event when game play of a level has failed
+    * VAR: levelNumber is the numeric value of the level failed*/
+    public void gameFailed(int levelNumber){
+
+    }
+
+    /*Call this event last game level is completed
+    * call it after gameCompleted and submitScore functions being called.
+    * VAR: lastLevelNumber is the numeric value of the level completed*/
+    public void gameEnded(int lastLevelNumber){
+
+    }
+
+    /*Call this event when user changes sound setting
+    * VAR: isSoundOn should be true if sound is switched ON
+    * VAR: isSoundOn should be false if sound is switched OFF*/
+    public void soundSettingChanged(boolean isSoundOn){
+
+    }
+
+    /*Call this event when the user exits the game by pressing back from the main screen*/
+    public void gameExit(){
+
+    }
+
+    /*Call this event when the game has loaded successfully and ready to interact with*/
+    public void gameLoaded(){
+
+    }
+
+    /*Call this event when the game has loaded successfully and ready to interact with
+    * VAR: percentage is the %value of the loaded game.
+    * If the game is having heavy resources to load then this shall be
+    * logged to pass the update to the loading screen*/
+    public void gameLoadingValue(int percentage){
+
+    }
+
+    /* Call this event to pass the game score after calling gameCompleted function.
+    * VAR: scoreValue is the integer value of the score given to the player */
+    public void submitScore(int scoreValue){
 
     }
 
